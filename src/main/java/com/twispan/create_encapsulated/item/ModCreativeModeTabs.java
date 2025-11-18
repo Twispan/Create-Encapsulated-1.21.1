46120f892f170dbb4abba2d27e7b646e76bef628
itsfirestorm <marcms244@gmail.com>
2025-11-18 11:51:01 +0100

    Added the "Aprijuices" creative tab

--- src/main/java/com/twispan/create_encapsulated/item/ModCreativeModeTabs.java
+++ src/main/java/com/twispan/create_encapsulated/item/ModCreativeModeTabs.java
@@ -11,30 +11,44 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
 public class ModCreativeModeTabs {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
-            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateEncapsulated.MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister
+            .create(Registries.CREATIVE_MODE_TAB, CreateEncapsulated.MODID);
 
-public static final Supplier<CreativeModeTab> POKEBALLCOMPONENTS_ITEMS_TAB = CREATIVE_MODE_TAB.register("pokeballcomponents_items_tab",
-        () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SPLITREDAPRICORN.get()))
-                .title(Component.translatable("creativetab.create_encapsulated.pokeballcomponents_items"))
-                .displayItems((itemDisplayParameters, output) -> {
-                    output.accept(ModItems.POKEBALLBASE);
-                    output.accept(ModItems.SPLITREDAPRICORN);
-                    output.accept(ModItems.SPLITBLUEAPRICORN);
-                    output.accept(ModItems.SPLITYELLOWAPRICORN);
-                    output.accept(ModItems.SPLITGRNAPRICORN);
-                    output.accept(ModItems.SPLITPNKAPRICORN);
-                    output.accept(ModItems.SPLITBLKAPRICORN);
-                    output.accept(ModItems.SPLITWHTAPRICORN);
-                    output.accept(ModItems.REDPAINT);
-                    output.accept(ModItems.BLUEPAINT);
-                    output.accept(ModItems.YELLOWPAINT);
-                    output.accept(ModItems.GREENPAINT);
-                    output.accept(ModItems.PINKPAINT);
-                    output.accept(ModItems.BLACKPAINT);
-                    output.accept(ModItems.WHITEPAINT);
-                }).build());
+    public static final Supplier<CreativeModeTab> POKEBALLCOMPONENTS_ITEMS_TAB = CREATIVE_MODE_TAB.register(
+            "pokeballcomponents_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SPLITREDAPRICORN.get()))
+                    .title(Component.translatable("creativetab.create_encapsulated.pokeballcomponents_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.POKEBALLBASE);
+                        output.accept(ModItems.SPLITREDAPRICORN);
+                        output.accept(ModItems.SPLITBLUEAPRICORN);
+                        output.accept(ModItems.SPLITYELLOWAPRICORN);
+                        output.accept(ModItems.SPLITGRNAPRICORN);
+                        output.accept(ModItems.SPLITPNKAPRICORN);
+                        output.accept(ModItems.SPLITBLKAPRICORN);
+                        output.accept(ModItems.SPLITWHTAPRICORN);
+                        output.accept(ModItems.REDPAINT);
+                        output.accept(ModItems.BLUEPAINT);
+                        output.accept(ModItems.YELLOWPAINT);
+                        output.accept(ModItems.GREENPAINT);
+                        output.accept(ModItems.PINKPAINT);
+                        output.accept(ModItems.BLACKPAINT);
+                        output.accept(ModItems.WHITEPAINT);
+                    }).build());
 
+    public static final Supplier<CreativeModeTab> APRIJUICES_TAB = CREATIVE_MODE_TAB.register(
+            "aprijuices_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SPICYAPRIJUICE.get()))
+                    .title(Component.translatable("creativetab.create_encapsulated.aprijuices"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.SPICYAPRIJUICE);
+                        output.accept(ModItems.DRYAPRIJUICE);
+                        output.accept(ModItems.SOURAPRIJUICE);
+                        output.accept(ModItems.BITTERAPRIJUICE);
+                        output.accept(ModItems.SWEETAPRIJUICE);
+                        output.accept(ModItems.SALTYAPRIJUICE);
+                        output.accept(ModItems.UMAMIAPRIJUICE);
+                    }).build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TAB.register(eventBus);
